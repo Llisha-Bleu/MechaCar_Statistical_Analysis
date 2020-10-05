94680ff892c8427f65be58febcd40206d9adcf08
Llisha-Bleu <66648340+Llisha-Bleu@users.noreply.github.com>
2020-10-04 23:32:17 -0400

    Updating Challenge file.

---
+++ R_Analysis/01_Demo/MechaCarChallenge.R
@@ -0,0 +1,41 @@
+#Deliverable 1 
+#use the library() function to load the dplyr package.
+library(dplyr)
+
+#import and read in the MechaCar_mpg.csv file as a dataframe.
+MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+head(MechaCar_mpg)
+
+#perform linear regression using the lm() function
+#create the multiple linear regression model
+lin <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)
+
+#summarize linear model
+summary(lin)
+
+#Deliverable 2
+#import and read in the Suspension_Coil.csv file as a table.
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+library(tidyverse)
+
+#create a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil's PSI column
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) 
+
+#create a lot_summary dataframe using the group_by() and the summarize() functionsn to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI column
+lot_summary <- Suspension_Coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) 
+
+#Deliverable 3
+
+#use the t.test() function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch
+?t.test()
+
+#determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch
+t.test(Suspension_Coil$PSI,mu=1500)
+
+#use the t.test() function and its subset() argument to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot3")$PSI, mu=1500)
+                                                                                                            
